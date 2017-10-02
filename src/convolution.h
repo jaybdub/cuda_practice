9c2e06304ae2b5b29a900d2a1f709ef7123505eb
John Welsh <jbmy13@gmail.com>
2017-10-02 16:17:42 -0400

    added 1D convolution

---
+++ src/convolution.h
@@ -0,0 +1,19 @@
+#ifndef CONVOLUTION_H
+#define CONVOLUTION_H
+
+__global__ void Convolve1D(float * x, int x_len,
+    float * w, int w_len, float * y) {
+  int y_idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (y_idx >= x_len)
+    return;
+
+  y[y_idx] = 0;
+  for (int w_idx = 0; w_idx < w_len; w_idx++) {
+    int x_idx = y_idx - w_len / 2 + w_idx;
+    if (0 <= x_idx && x_idx < x_len)
+      y[y_idx] += w[w_idx] * x[x_idx];
+  }
+}
+
+#endif  // CONVOLUTION_H
---
+++ test/test_convolution.cu
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "src/convolution.h"
+
+using namespace std;
+
+void Print(float * x, int x_len) {
+  for (int i = 0; i < x_len; i++)
+    cout << x[i] << endl;
+}
+
+void TestConvolution1D() {
+  const size_t N = 20;
+  float a[N], b[N];
+  for (int i = 0; i < N; i++)
+    a[i] = i;
+  const size_t N_w = 5;
+  float w[N_w] = { -2, -1, 0, 1, 2 };
+  float * d_a, * d_b, * d_w;   
+  const size_t size = N * sizeof(float);
+  cudaMalloc(&d_a, size);
+  cudaMalloc(&d_b, size);
+  cudaMalloc(&d_w, N_w * sizeof(float));
+  cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice); 
+  cudaMemcpy(d_w, w, N_w * sizeof(float), cudaMemcpyHostToDevice);
+  Convolve1D<<<N, 1>>>(d_a, N, d_w, N_w, d_b);
+  cudaMemcpy(b, d_b, size, cudaMemcpyDeviceToHost);
+  Print(b, N);  
+  cudaFree(d_a);
+  cudaFree(d_b);
+}
+
+int main() {
+  TestConvolution1D();
+  return 0;
+}
