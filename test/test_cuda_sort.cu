8480250c0ca596675e93b6d143e2761fec6a39ab
John Welsh <jbmy13@gmail.com>
2017-10-03 12:52:40 -0400

    fixed cuda sort test bug

--- test/test_cuda_sort.cu
+++ test/test_cuda_sort.cu
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
   int nums[5] = {5, 3, 2, 1, 4};
   int sorted[5];
-  cuda_sort(nums, sorted, 5);
+  cuda_merge_sort(nums, sorted, 5);
   Print(sorted, 5);
 
   return 0;
